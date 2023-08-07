013628e78942db67916c9dfa2d5660b3bf111acf
Yi Cheng Lee <leeyicheng10@gmail.com>
2023-08-07 23:01:47 +0800

    added generate new array when an algorithm is selected

--- src/Component/Toolbar/DropdownComponents/Algorithm/algorithm.component.jsx
+++ src/Component/Toolbar/DropdownComponents/Algorithm/algorithm.component.jsx
@@ -20,19 +20,25 @@ import {
   Item,
 } from '../DropdownContainer/dropdownContainer.styles';
 import OutsideAlerter from '../OutsideAlerter/outside.component';
+import { generateNewArray } from '../../../../Store/array/array.action';
+import { selectArrayLength } from '../../../../Store/array/array.selector';
 
 const Algorithm = () => {
   const dispatch = useDispatch();
 
   const algorithm = useSelector(selectAlgorithm);
   const algoDropdownOpen = useSelector(selectIsAlgoDropdownOpen);
+  const len = useSelector(selectArrayLength);
   const algoList = Object.values(ALGO_TYPES);
 
   const toggleDropdown = () =>
     dispatch(setIsAlgoDropdownOpen(!algoDropdownOpen));
 
+  const generateNewArr = () => dispatch(generateNewArray(len));
+
   const changeAlgorithm = (algo) => {
     toggleDropdown();
+    generateNewArr();
     const newAlgo = Object.keys(ALGO_TYPES).find(
       (key) => ALGO_TYPES[key] === algo
     );
